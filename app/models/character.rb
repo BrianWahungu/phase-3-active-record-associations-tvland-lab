c4b1a2de01100b3dbd9606d48adbfa29640d4ddf
Brian Wahungu <cepteinhawks@gmail.com>
2023-02-24 07:56:20 +0300

    completed assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
-end
+    belongs_to :show
+    belongs_to :actor
+ 
+   def say_that_thing_you_say
+     "#{self.name} always says: #{self.catchphrase}"
+   end
+    
+ #    def actor
+ #     self.actor
+ #    end
+ end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,12 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+      # t.belongs_to :actor, foreign_key: true
+      # t.belongs_to :show, foreign_key: true
+    end
   end
 end
